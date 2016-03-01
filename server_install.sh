f89f30979cf0cce1c71fb3131e498291dc3d47b7
Rodney Cobb <roacobb@gmail.com>
2016-03-01 09:55:00 -0500

    Added git nano wget to install

--- server_install.sh
+++ server_install.sh
@@ -12,3 +12,4 @@ sudo aptitude -y install apache2 apache2.2-common apache2-mpm-prefork apache2-ut
 sudo aptitude -y install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd \
 php5-imagick php5-mcrypt php5-memcache php5-mhash php5-mysql php5-pspell php5-snmp \
 php5-sqlite php5-xmlrpc php5-xsl
+sudo apt-get -y install nano git wget
